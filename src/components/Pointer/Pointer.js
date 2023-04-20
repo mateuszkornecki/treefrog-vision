228c7c4d2f93ad108305cce484e2b77b246b37a9
Mateusz Kornecki <mateuszkornecki@MacBook-Pro-Mateusz.local>
2023-04-20 10:21:32 +0200

    added Pointer component, reorginised project structure

---
+++ src/components/Pointer/Pointer.js
@@ -0,0 +1,6 @@
+import './Pointer.css';
+function Pointer() {
+  return <div className={"Pointer"}/>
+}
+
+export default Pointer;
---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import logo from '../../logo.svg';
+import Pointer from "../Pointer/Pointer";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Pointer/>
+    </div>
+  );
+}
+
+export default App;
