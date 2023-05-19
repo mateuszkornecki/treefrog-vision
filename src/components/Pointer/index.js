930ca7754282f62320a3b43fd50a9a1e5d6aae98
Mateusz Kornecki <mateusz.kornecki@hey.com>
2023-05-19 09:45:08 +0200

    Renamed reset props

--- src/components/Pointer/index.js
+++ src/components/Pointer/index.js
@@ -1,6 +1,6 @@
 import './index.css';
-function Pointer({bounce, pause,restart}) {
-  return <div className={`Pointer ${restart ? "" : "bounce"} ${pause ? 'pause' : 'running'}`}/>
+function Pointer({bounce, pause,freezeAndCenter}) {
+  return <div className={`Pointer ${freezeAndCenter ? "" : "bounce"} ${pause ? 'pause' : 'running'}`}/>
 }
 
 export default Pointer;
