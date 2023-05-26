9f03ea57a580d51e2d46d64aa53ca20e23af908f
Mateusz Kornecki <mateusz.kornecki@hey.com>
2023-05-26 08:42:51 +0200

    Added ThemeSetter component
    Signed-off-by: Mateusz Kornecki <mateusz.kornecki@hey.com>

---
+++ src/components/ThemeSetter/index.js
@@ -0,0 +1,33 @@
+import './ThemeSetter.css';
+import ThemeInput from "../ThemeInput";
+
+function ThemeSetter({onThemeChange, colorPairs}) {
+    function getCorespondingBackground(pointerColor) {
+        let correspondingbackgroundColor = null;
+        colorPairs.forEach(pair => {
+            if (pair.pointer == pointerColor) {
+                correspondingbackgroundColor = pair.background;
+            }
+        })
+
+        return correspondingbackgroundColor || null;
+    }
+    function handleThemeChange(e) {
+        onThemeChange({
+                pointer:e.target.value,
+                background: getCorespondingBackground(e.target.value)
+            });
+    }
+    return <fieldset  className={"options"}>
+        <legend>Theme</legend>
+        {colorPairs.map(
+            (colorPair,i) => <ThemeInput
+                key={colorPair.pointer+colorPair.background}
+                theme={colorPair}
+                positionNumber={i}
+                onThemeChange={handleThemeChange}
+            />
+        )}
+    </fieldset>
+    }
+export default  ThemeSetter;
