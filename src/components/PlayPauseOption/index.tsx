e7e38e7ec60e58a117fecbfa84e54aed3115bdad
Mateusz Kornecki <mateusz.kornecki@hey.com>
2023-06-02 08:39:34 +0200

    refactor: migrated PlayPauseOption component index file to index.tsx
    Signed-off-by: Mateusz Kornecki <mateusz.kornecki@hey.com>

--- src/components/PlayPauseOption/index.tsx
+++ src/components/PlayPauseOption/index.tsx
@@ -1,5 +1,6 @@
 import PauseCircleOutline from "@mui/icons-material/PauseCircleOutline";
 import PlayCircleOutline from "@mui/icons-material/PlayCircleOutline";
+import React from "react";
 
 function PlayPauseOption({isRunning, onClick}) {
     return isRunning ?
