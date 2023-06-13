0e9cf1c752c9d1b93004013b3e1b2a4a64f5553a
Mateusz Kornecki <mateusz.kornecki@hey.com>
2023-06-13 10:50:38 +0200

    feat(ts):added PlayPauseOptionProps type
    Signed-off-by: Mateusz Kornecki <mateusz.kornecki@hey.com>

--- src/components/PlayPauseOption/index.tsx
+++ src/components/PlayPauseOption/index.tsx
@@ -2,7 +2,11 @@ import PauseCircleOutline from "@mui/icons-material/PauseCircleOutline";
 import PlayCircleOutline from "@mui/icons-material/PlayCircleOutline";
 import React from "react";
 
-function PlayPauseOption({isRunning, onClick}) {
+type PlayPauseOptionProps = {
+    isRunning:boolean,
+    onClick: ()=>void
+}
+function PlayPauseOption({isRunning, onClick}: PlayPauseOptionProps) {
     return isRunning ?
         <PauseCircleOutline
             onClick={onClick}
