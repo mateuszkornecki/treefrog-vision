818fc7f81b938489c24928f287a77186d412a9c3
Mateusz Kornecki <mateusz.kornecki@hey.com>
2023-06-19 20:25:19 +0200

    feat: added changePointerSize function
    Signed-off-by: Mateusz Kornecki <mateusz.kornecki@hey.com>

--- src/components/Pointer/index.tsx
+++ src/components/Pointer/index.tsx
@@ -15,6 +15,20 @@ function Pointer({paused, freezeAndCenter, color, size}: PointerProps) {
     animationPlayState: paused ? "paused" : "running",
     backgroundColor: color,
   }
+
+  function changePointerSize(newSize: PointerSize): void {
+    if (newSize === 'tiny') {
+      document.documentElement.style.setProperty('--pointer-size', `75px`);
+    } else if (newSize === 'large') {
+      document.documentElement.style.setProperty('--pointer-size', `300px`);
+    } else {
+      document.documentElement.style.setProperty('--pointer-size', `175px`);
+    }
+  }
+
+  useEffect(() => {
+    changePointerSize(size);
+  }, [size]);
   return <div
       className={`Pointer ${freezeAndCenter ? "":"animatePointer"}`}
   style={pointerStyles}
