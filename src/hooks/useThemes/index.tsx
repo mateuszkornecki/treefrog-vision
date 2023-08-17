eb16cebb58122d668102df5b06ce732a38549a4a
Mateusz Kornecki <mateusz.kornecki@hey.com>
2023-08-17 17:44:49 +0200

    chore(json): replaced 'initialThemes' var with the content imported from the json file
    Signed-off-by: Mateusz Kornecki <mateusz.kornecki@hey.com>

--- src/hooks/useThemes/index.tsx
+++ src/hooks/useThemes/index.tsx
@@ -1,14 +1,10 @@
 import {useState} from "react";
 import {TTheme} from "../../components/ThemeSetter";
+import data from "../../themes.json"
 
-const initialThemes: TTheme[] = [
-    {pointer: "red", background: "black"},
-    {pointer: "hotpink", background: "lightblue"},
-    {pointer: "blue", background: "green"},
-];
 
 function useThemes() {
-    const [themes, setThemes] = useState(initialThemes);
+    const [themes, setThemes] = useState<TTheme[]>(data);
 
     return {themes, setThemes};
 }
