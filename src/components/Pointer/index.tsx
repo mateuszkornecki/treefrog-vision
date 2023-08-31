72c491b6b6f9a1f0a42bb945342a14ac976eb228
Mateusz Kornecki <mateusz.kornecki@hey.com>
2023-08-31 08:21:57 +0200

    deat(1.1.1A): modified Pointer to support exercise 1.1.1A

--- src/components/Pointer/index.tsx
+++ src/components/Pointer/index.tsx
@@ -2,9 +2,8 @@ import React, {AnimationEvent, useEffect, useState} from 'react';
 import './Pointer.css';
 
 type TPointerSize = 'tiny' | 'regular' | 'large';
-type TDirectionMode = 'modeLeft' | 'modeLeftReturn' | 'modeRight' | 'modeRightReturn' | 'modeBoth';
-type TAnimationIterationCount = 'infinite' | 1;
-type TAnimationDuration = '30s' | '5s' | '2s'
+type TDirectionMode = 'modeCL' | 'modeLC' | 'modeCR'| 'modeRC';
+type TAnimationDuration = '30s'
 
 type TPointerProps = {
   paused: boolean,
@@ -14,37 +13,16 @@ type TPointerProps = {
   directionMode: TDirectionMode,
 }
 
-function Pointer({paused, freezeAndCenter, color, size, directionMode}: TPointerProps) {
-  const [animationDuration, setAnimationDuration] = React.useState<TAnimationDuration>('30s');
+function Pointer({ color, directionMode}: TPointerProps) {
   const [mode, setMode] = useState<TDirectionMode>(directionMode);
-  const [animationIterationCount, setAnimationIterationCount] = React.useState<TAnimationIterationCount>('infinite');
 
   const pointerStyles = {
     animationName: mode,
-    animationPlayState: paused ? "paused" : "running",
     backgroundColor: color,
-    animationDuration: animationDuration,
-    animationIterationCount: animationIterationCount,
+    animationDuration: '30s',
+    animationIterationCount: 1,
+    animationDelay: '5s'
   }
-
-  function changePointerSize(newSize: TPointerSize): void {
-    if (newSize === 'tiny') {
-      document.documentElement.style.setProperty('--pointer-size', `75px`);
-    } else if (newSize === 'large') {
-      document.documentElement.style.setProperty('--pointer-size', `300px`);
-    } else {
-      document.documentElement.style.setProperty('--pointer-size', `175px`);
-    }
-  }
-  useEffect(() => {
-    // we need to set it when for example 'modeBoth' button is clicked
-    if (mode === 'modeBoth') {
-      setAnimationIterationCount('infinite');
-    } else {
-      setAnimationIterationCount(1)
-    }
-  }, [mode])
-
   // We need to set it everytime the props changes,
   // mode is just an element of the internal state
   useEffect(() => {
@@ -52,41 +30,23 @@ function Pointer({paused, freezeAndCenter, color, size, directionMode}: TPointer
   }, [directionMode]);
 
   function handleAnimationEnd(event: AnimationEvent<HTMLDivElement>): void {
-    // inis setfinite should be used only when modeBoth is set!
-    // setAnimationIterationCount(1);
-
-    if (event.animationName === 'modeLeft') {
-      setMode('modeLeftReturn');
-      setAnimationDuration('2s');
-    } else if (event.animationName === 'modeRight') {
-      setMode('modeRightReturn');
-      setAnimationDuration('2s');
-    } else if (event.animationName === 'modeLeftReturn') {
-      setMode('modeLeft');
-      setAnimationDuration('30s');
-    } else if (event.animationName === 'modeRightReturn') {
-      setMode('modeRight');
-      setAnimationDuration('30s');
-    }
+    if (event.animationName === 'modeCL') {
+      setMode('modeLC');
+    } else if (event.animationName === 'modeLC') {
+      setMode('modeCR');
+    } else if (event.animationName === 'modeRC') {
+      setMode('modeCL');
+    } else if (event.animationName === 'modeCR') {
+    setMode('modeRC');
   }
+  };
 
-  useEffect(() => {
-    if (mode !== 'modeBoth') {
-      setAnimationIterationCount(1);
-    } else {
-      setAnimationIterationCount('infinite');
-    }
-  }, [mode]);
-
-  useEffect(() => {
-    changePointerSize(size);
-  }, [size]);
   return <div
-      className={`Pointer ${freezeAndCenter ? "" : "animatePointer"}`}
+      className={"Pointer animatePointer"}
       style={pointerStyles}
       onAnimationEnd={handleAnimationEnd}
   />
 }
 
 export default Pointer;
-export {TPointerSize, TDirectionMode}
+export {TDirectionMode}
