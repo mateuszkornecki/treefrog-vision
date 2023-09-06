df20c8d55a810d565328b98d109030a3c4dc812d
Mateusz Kornecki <mateusz.kornecki@hey.com>
2023-09-06 09:49:21 +0200

    fix: corrected the incorrect import path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Pointer from "../components/Pointer";
 import useTheme from "../hooks/useTheme";
-import '../app/page.css';
+import './page.css';
 
 function App() {
   const {theme,changeToRandomTheme} = useTheme();
