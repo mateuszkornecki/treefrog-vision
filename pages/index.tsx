1c0910cf5d9fcf6b83449caf4994c9a92b766acf
Mateusz Kornecki <mateusz.kornecki@hey.com>
2023-09-08 10:05:36 +0200

    refactor:renamed and moved the page.tsx into index.tsx and moved it to the pages directory

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Pointer from "../components/Pointer";
 import useTheme from "../hooks/useTheme";
-import './page.css';
+import '../styles/homePage.css';
 
 function App() {
   const {theme,changeToRandomTheme} = useTheme();
