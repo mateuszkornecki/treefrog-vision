d967a91306d81a4f3729f7660f95095fe14daedc
Mateusz Kornecki <mateusz.kornecki@hey.com>
2023-09-29 16:05:29 +0200

    Revert "refactor: removed redundant redirection"
    This reverts commit 4ecda30df818fb4b46e8b7b2d3282e4065b25f95.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@ module.exports = {
     async redirects() {
         return [
             {
+                source: '/',
+                destination: '/train?exercise=1_1',
+                permanent: true,
+            },
+            {
                 source: '/debug',
                 destination: '/train?exercise=O1&iterationTime=5s&delay=3s',
                 permanent: true,
