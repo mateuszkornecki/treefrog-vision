943b11cf1372a148212d257eed9dfa6523161211
Mateusz Kornecki <mateusz.kornecki@hey.com>
2023-09-29 18:38:46 +0200

    feat: added a redirection to the train directory

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@ module.exports = {
     async redirects() {
         return [
             {
+                source: '/',
+                destination: '/train',
+                permanent: true,
+            },
+            {
                 source: '/debug',
                 destination: '/train?exercise=O1&iterationTime=5s&delay=3s',
                 permanent: true,
