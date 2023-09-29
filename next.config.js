9b2f9a48b8bade52287353065563dfd6563f11ab
Mateusz Kornecki <mateusz.kornecki@hey.com>
2023-09-29 17:27:27 +0200

    feat: deleted redundant redirection

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
         return [
             {
                 source: '/',
-                destination: '/train?exercise=1_1',
+                destination: '/',
                 permanent: true,
             },
             {
