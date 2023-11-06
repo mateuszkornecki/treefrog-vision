39690f006b9e2b509561e8745687e6d8f9709c70
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2023-11-06 15:36:42 +0100

    Revert "feat: added cypress config file"
    This reverts commit cf02614a021affe9591169c16b0bb715297c45ce.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,16 @@
+import { defineConfig } from "cypress";
 
-module.exports = {
-    projectId: "gxxc78",
-}
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+});
