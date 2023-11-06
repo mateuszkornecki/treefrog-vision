68b1b3f8e10e95f85d0fb12004fd5b9d306bac3f
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2023-11-06 15:52:23 +0100

    feat: added cypress config file

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: 'gxxc78',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
