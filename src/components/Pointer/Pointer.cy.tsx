ab0b64a7d613940befdc1d46da7a33663cb23466
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2023-11-15 16:55:36 +0100

    feat: added tests for Pointer properties

--- src/components/Pointer/Pointer.cy.tsx
+++ src/components/Pointer/Pointer.cy.tsx
@@ -2,17 +2,20 @@ import React from 'react'
 import Pointer from './'
 
 describe('Pointer', () => {
-  it('color property is changing Pointer color basing on it input.', () => {
+  it('Properties work correctly', () => {
 
     const pointerColor = "rgb(255, 105, 180)";
     const currentExercise = "O1";
     const iterationTime = "10s";
     const delayTime = "3s";
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<Pointer  color={pointerColor} exercise={currentExercise} iterationTime={iterationTime} delay={delayTime}/>);
+    cy.mount(<Pointer  color={pointerColor} exercise={currentExercise} iterationTime={iterationTime} delay={delayTime} />);
 
     cy.get('.Pointer')
         .should('have.css', 'background-color', pointerColor)
+        .should('have.css', 'animation-delay', delayTime)
+        .should('have.css', 'animation-duration', iterationTime)
+        .should('have.css', 'animation-play-state', "running")
 
 
 
