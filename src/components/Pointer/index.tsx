1adebf952eeaf48117ab59a58d16a32999d7f426
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2023-11-20 15:16:09 +0100

    refactor: extracted getNextDirectionMode function to the standalone file

--- src/components/Pointer/index.tsx
+++ src/components/Pointer/index.tsx
@@ -3,6 +3,7 @@
 import React, {AnimationEvent, useCallback, useContext, useEffect, useState} from 'react';
 import settingContext from "@/context/ConfigsContext";
 import {TExerciseNumber} from "@/app/exercise/[slug]/page";
+import getNextDirectionMode from "@/utils/getNextDirectionMode";
 
 type TPointerSize = 'tiny' | 'regular' | 'large';
 type TDirectionMode = 'centerToLeft' | 'leftToCenter' | 'centerToRight'| 'rightToCenter'| 'leftToRight' | 'rightToLeft' | 'centerToTop' | 'topToCenter'
@@ -16,64 +17,6 @@ type TPointerProps = {
   onClick?: () => void,
   paused?: boolean,
   color: string
-}
-
-function getNextDirectionMode(exercise: TExerciseNumber, previousDirectionMode: string): TDirectionMode  {
-
-  const INVALID_DIRECTION_MODE_ERROR  = "Invalid directionMode!"
-  if(exercise === "O1") {
-    if (previousDirectionMode === 'centerToLeft') {
-      return 'leftToCenter';
-    } else if (previousDirectionMode === 'leftToCenter') {
-      return 'centerToRight';
-    } else if (previousDirectionMode === 'rightToCenter') {
-      return 'centerToLeft';
-    } else if (previousDirectionMode === 'centerToRight') {
-      return 'rightToCenter';
-    } else {
-      throw new Error(INVALID_DIRECTION_MODE_ERROR);
-    }
-  } else if(exercise === "O2") {
-    if (previousDirectionMode === 'centerToLeft') {
-      return 'leftToRight';
-    } else if (previousDirectionMode === 'leftToRight') {
-      return 'rightToLeft';
-    } else if (previousDirectionMode === 'rightToLeft') {
-      return 'leftToRight';
-    } else {
-      throw new Error(INVALID_DIRECTION_MODE_ERROR);
-    }
-  } else if(exercise === "O3") {
-    if (previousDirectionMode === 'centerToLeft') {
-      return 'leftToCenter';
-    } else if (previousDirectionMode === 'leftToCenter') {
-      return 'centerToLeft';
-    } else {
-      throw new Error(INVALID_DIRECTION_MODE_ERROR);
-    }
-  } else if(exercise === "O4") {
-    if (previousDirectionMode === 'centerToRight') {
-      return 'rightToCenter';
-    } else if (previousDirectionMode === 'rightToCenter') {
-      return 'centerToRight';
-    } else {
-      throw new Error(INVALID_DIRECTION_MODE_ERROR);
-    }
-  } else if(exercise === "O5") {
-    if (previousDirectionMode === "centerToTop") {
-      return "topToCenter";
-    } else if (previousDirectionMode === "topToCenter") {
-      return "centerToBottom";
-    } else if (previousDirectionMode === "centerToBottom") {
-      return "bottomToCenter";
-    } else if (previousDirectionMode === "bottomToCenter") {
-      return "centerToTop";
-    } else {
-      throw new Error(INVALID_DIRECTION_MODE_ERROR);
-    }
-  } else {
-    throw new Error("Incorrect exercise number!")
-  }
 }
 
 
@@ -230,5 +173,4 @@ function Pointer({ color, onClick, paused,iterationTime, delay, exercise}: TPoin
 }
 
 export default Pointer;
-export {getNextDirectionMode};
 export type {TDirectionMode, TSeconds};
