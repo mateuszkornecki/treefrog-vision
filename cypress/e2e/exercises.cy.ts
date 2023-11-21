16333ebe6049ea1ee3d48eecfbf1e0e574ea4c38
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2023-11-21 12:52:30 +0100

    refactor: deleted redundant, copy-pasted code line

--- cypress/e2e/exercises.cy.ts
+++ cypress/e2e/exercises.cy.ts
@@ -2,8 +2,6 @@ describe('exercises', () => {
     it('renders Pointer', () => {
         cy.visit(`http://localhost:3000/exercise/O1`)
         cy.get('.Pointer')
-            .should('have.css', 'animation-duration', '30s')
-            .should('have.css', 'animation-delay', '5s')
     })
 
     it('sets animation parameters specified in the given URL', () => {
