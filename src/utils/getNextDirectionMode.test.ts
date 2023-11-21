c92d5cb8d09471124ddf65f6e361a134b5ab81f4
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2023-11-21 12:57:43 +0100

    feat: added test that checks incorrect exerciseNumber argument

--- src/utils/getNextDirectionMode.test.ts
+++ src/utils/getNextDirectionMode.test.ts
@@ -2,7 +2,10 @@ import getNextDirectionMode from "@/utils/getNextDirectionMode";
 import {describe, expect, test} from '@jest/globals';const ALPACA_THAT_THROWS_ERRORS = 'this alpaca will throw an error';
 
 describe('getNextDirectionMode function', () => {
-    test('returns "All previousDirectionMode for exercise O1 returns a correct output', () => {
+    test('returns "Throws an error for incorrect exercise number', () => {
+        expect(() => getNextDirectionMode("666", 'centerToLeft')).toThrow();
+    });
+    test('returns "All previousDirectionModes for exercise O1 returns a correct output', () => {
         expect(getNextDirectionMode("O1", 'centerToLeft')).toBe('leftToCenter');
         expect(getNextDirectionMode("O1", 'leftToCenter')).toBe('centerToRight');
         expect(getNextDirectionMode("O1", 'rightToCenter')).toBe('centerToLeft');
