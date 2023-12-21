d8c5359e97ec21af12bf50a807ab3f8bea9372b2
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2023-12-21 19:20:06 +0100

    refactor(semicolons): delete semicolons

--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -1,27 +1,26 @@
 "use client"
 
-import {ReactNode, useEffect, useState} from "react";
-import THEMES from "@/THEMES.json";
-import ThemeContext from "@/context/ThemeContext";
-import {TThemeName} from "@/types/TThemeName";
+import {ReactNode, useEffect, useState} from "react"
+import THEMES from "@/THEMES.json"
+import ThemeContext from "@/context/ThemeContext"
+import {TThemeName} from "@/types/TThemeName"
 
 type TProvidersProps = {
     children: ReactNode
 }
 
 const Providers =({children}: TProvidersProps) => {
-
-    const [currentThemeName, setCurrentName] = useState<TThemeName>("default");
-    const [currentTheme, setCurrentTheme] = useState(THEMES[currentThemeName]);
+    const [currentThemeName, setCurrentName] = useState<TThemeName>("default")
+    const [currentTheme, setCurrentTheme] = useState(THEMES[currentThemeName])
 
     function changeThemeTo(newThemeName: TThemeName): void {
-        console.log(newThemeName)
-        setCurrentTheme(THEMES[newThemeName]);
+        setCurrentName(newThemeName)
+        setCurrentTheme(THEMES[newThemeName])
     }
 
     useEffect(() => {
-        changeThemeTo(currentThemeName);
-    }, [currentThemeName]);
+        changeThemeTo(currentThemeName)
+    }, [currentThemeName])
 
     const appState = {
         currentTheme,
@@ -33,8 +32,8 @@ const Providers =({children}: TProvidersProps) => {
         <ThemeContext.Provider value={appState}>
             {children}
         </ThemeContext.Provider>
-    );
+    )
 }
-export default Providers;
+export default Providers
 
-export {ThemeContext};
+export {ThemeContext}
