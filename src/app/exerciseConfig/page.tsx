5f2884557dd9778d442f23e1a442ac69d788e0d8
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2024-01-09 09:25:45 +0100

    refactor: replacehardcoded elementswith custom components

--- src/app/exerciseConfig/page.tsx
+++ src/app/exerciseConfig/page.tsx
@@ -1,19 +1,19 @@
 "use client"
 
 import useTheme from "@/hooks/useTheme";
+import ConfigItem from "@/components/ConfigItem"
+import Configs from "@/components/Configs"
 
 function Page(){
     const {theme} = useTheme()
 
     return (
-        <main className={"exerciseConfig"}>
-            <div className="configs">
-                <h1 className={"configsHeading"}>Ustawienia ćwiczenia</h1>
-                <h2 className={"configHeading"}>Kolorystyka ćwiczenia:</h2>
-                <h2 className={"configHeading"}>Wielkość obiektu:</h2>
-                <h2 className={"configHeading"}>Prędkość poruszania się obiektu:</h2>
-                <h2 className={"configHeading"}>Czas zatrzymania obiektu:</h2>
-            </div>
+        <main>
+            <Configs>
+                <ConfigItem name={"Prędkość poruszania się obiektu:"}>
+                    <input type={'range'}/>
+                </ConfigItem>
+            </Configs>
         </main>
     )
 }
