54e98a44564d76438daf705076371dd3e891f8b2
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2024-01-17 14:37:14 +0100

    refactor: delete unused variable

--- src/hooks/useConfig/index.ts
+++ src/hooks/useConfig/index.ts
@@ -3,7 +3,6 @@ import ConfigContext from "@/context/ConfigContext";
 import {TConfig} from "@/types/TConfig"
 
 export default function useConfig(): TConfig {
-    const ala = useContext(ConfigContext)
     // IMPORTANT NOTE: Must be used inside component that is wrapped with the ConfigContextProvider
     return useContext(ConfigContext)
 
