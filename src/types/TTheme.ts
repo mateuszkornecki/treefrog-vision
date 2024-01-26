5f074b514e5c26d3b57451ee4e38b53162f72b59
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2024-01-26 20:32:29 +0100

    refactor(TTheme): chsnge TTheme type to not rely on THEMES.ts file

--- src/types/TTheme.ts
+++ src/types/TTheme.ts
@@ -1,7 +1,15 @@
-import {THEMES} from "@/constants/THEMES"
+type TThemeName = "default" | "bw"
 
-type TThemeName = keyof typeof THEMES
-type TTheme = typeof THEMES[TThemeName]
-type TThemes = typeof THEMES
+type TTheme = {
+    name: TThemeName,
+    // TODO TPointerColor
+    //TODO TBackgroundColor
+    pointerColor:string,
+    backgroundColor: string
+}
+
+type TThemes = {[k in TThemeName]: TTheme }
 
 export type {TTheme, TThemeName, TThemes}
+
+//TODO Create issue to refactor CONFIGS.JSON in the same way like THEMES.ts
