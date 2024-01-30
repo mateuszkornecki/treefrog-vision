88e75441eac9fa914db95e57392cbc6ca19bdfa7
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2024-01-30 12:47:34 +0100

    refactor(TODO-comments): deleted not  up to date todo comments

--- src/types/TTheme.ts
+++ src/types/TTheme.ts
@@ -2,8 +2,6 @@ type TThemeName = "default" | "bw"
 
 type TTheme = {
     name: TThemeName,
-    // TODO TPointerColor
-    //TODO TBackgroundColor
     pointerColor:string,
     backgroundColor: string
 }
@@ -11,5 +9,3 @@ type TTheme = {
 type TThemes = {[k in TThemeName]: TTheme }
 
 export type {TTheme, TThemeName, TThemes}
-
-//TODO Create issue to refactor CONFIGS.JSON in the same way like THEMES.ts
