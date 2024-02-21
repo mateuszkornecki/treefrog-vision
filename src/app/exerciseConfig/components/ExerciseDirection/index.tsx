959e558135b55fd7a2c363255646613b78596588
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2024-02-21 16:06:07 +0100

    refactor(exerciseDirection): clean up component code

--- src/app/exerciseConfig/components/ExerciseDirection/index.tsx
+++ src/app/exerciseConfig/components/ExerciseDirection/index.tsx
@@ -2,20 +2,20 @@ import React, {ReactElement} from "react"
 function ExerciseDirection(): ReactElement {
     return (
         <>
-            <div className="exerciseTheme__container">
-                <div className="exerciseTheme__LTItem">LT</div>
-                <div className="exerciseTheme__RCItem">RC</div>
-                <div className="exerciseTheme__RTItem">RT</div>
-                <div className="exerciseTheme__RBItem">RB</div>
-                <div className="exerciseTheme__CBItem">CB</div>
-                <div className="exerciseTheme__CTItem">CT</div>
-                <div className="exerciseTheme__LCItem">LC</div>
-                <div className="exerciseTheme__LBItem">LB</div>
-                <div className="exerciseTheme__CCItem">CC</div>
+            <div className="exerciseDirection__container">
+                <div className="exerciseDirection__LTItem">LT</div>
+                <div className="exerciseDirection__RCItem">RC</div>
+                <div className="exerciseDirection__RTItem">RT</div>
+                <div className="exerciseDirection__RBItem">RB</div>
+                <div className="exerciseDirection__CBItem">CB</div>
+                <div className="exerciseDirection__CTItem">CT</div>
+                <div className="exerciseDirection__LCItem">LC</div>
+                <div className="exerciseDirection__LBItem">LB</div>
+                <div className="exerciseDirection__CCItem">CC</div>
             </div>
             <style jsx>
                 {`
-                    .exerciseTheme__container {
+                    .exerciseDirection__container {
                         display: grid;
                         // TODO Check if height and width are set correctly
                         grid-template-columns: 6rem 6rem 6rem 6rem 6rem;
@@ -39,47 +39,21 @@ function ExerciseDirection(): ReactElement {
                     }
 
                     .exerciseTheme__RCItem { grid-area: 3 / 5 / 4 / 6; }
-
                     .exerciseTheme__CCItem { grid-area: 3 / 3 / 4 / 4; }
-
                     .exerciseTheme__RTItem { grid-area: 1 / 5 / 2 / 6; }
-
                     .exerciseTheme__RBItem { grid-area: 5 / 5 / 6 / 6; }
-
                     .exerciseTheme__CBItem { grid-area: 5 / 3 / 6 / 4; }
-                    
                     .exerciseTheme__CTItem { grid-area: 1 / 3 / 2 / 4; }
-
                     .exerciseTheme__LCItem { grid-area: 3 / 1 / 4 / 2; }
-
                     .exerciseTheme__LBItem { grid-area: 5 / 1 / 6 / 2; }
-
                     .exerciseTheme__CCItem { grid-area: 3 / 3 / 4 / 4; }
-
-
-                    html, body , .exerciseTheme__container {
-                        height: 100%;
-                        margin: 0;
-                    }
-
-                    /* For presentation only, no need to copy the code below */
-
-                    .exerciseTheme__container * {
-                        border: 1px solid red;
-                        position: relative;
-                    }
-
-                    .exerciseTheme__container *:after {
-                        /* content:attr(class); */
-                        position: absolute;
-                        top: 0;
-                        left: 0;
-                        width: 100%;
-                        height: 100%;
-                        display: grid;
-                        align-items: center;
-                        justify-content: center;
-                    }
+                    
+               // TODO Delete the presentation code after the component will be ready
+                    
+                    // Presentation only!
+//                    .exerciseTheme__container * {
+//                        //border: 1px solid red;
+//                    }
                 `}
             </style>
         </>
