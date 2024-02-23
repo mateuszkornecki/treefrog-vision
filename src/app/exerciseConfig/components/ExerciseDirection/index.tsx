ba1c4fcf1bcd17357a8d1d62f39945e4d26f4370
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2024-02-23 13:24:10 +0100

    refactor(exerciseDirection): adjust classNames

--- src/app/exerciseConfig/components/ExerciseDirection/index.tsx
+++ src/app/exerciseConfig/components/ExerciseDirection/index.tsx
@@ -3,15 +3,15 @@ function ExerciseDirection(): ReactElement {
     return (
         <>
             <div className="exerciseDirection__container">
-                <div className="exerciseDirection__LTItem">LT</div>
-                <div className="exerciseDirection__RCItem">RC</div>
-                <div className="exerciseDirection__RTItem">RT</div>
-                <div className="exerciseDirection__RBItem">RB</div>
-                <div className="exerciseDirection__CBItem">CB</div>
-                <div className="exerciseDirection__CTItem">CT</div>
-                <div className="exerciseDirection__LCItem">LC</div>
-                <div className="exerciseDirection__LBItem">LB</div>
-                <div className="exerciseDirection__CCItem">CC</div>
+                <div className="exerciseDirection__item exerciseDirection__item--LT">LT</div>
+                <div className="exerciseDirection__item exerciseDirection__item--RC">RC</div>
+                <div className="exerciseDirection__item exerciseDirection__item--RT">RT</div>
+                <div className="exerciseDirection__item exerciseDirection__item--RB">RB</div>
+                <div className="exerciseDirection__item exerciseDirection__item--CB">CB</div>
+                <div className="exerciseDirection__item exerciseDirection__item--CT">CT</div>
+                <div className="exerciseDirection__item exerciseDirection__item--LC">LC</div>
+                <div className="exerciseDirection__item exerciseDirection__item--LB">LB</div>
+                <div className="exerciseDirection__item exerciseDirection__item--CC">CC</div>
             </div>
             <style jsx>
                 {`
@@ -24,35 +24,41 @@ function ExerciseDirection(): ReactElement {
                         border: 2px solid #E6E0E9;
                         border-radius: 0.5rem;
                     }
-
-                    .exerciseDirection__LTItem {
+                    
+                    .exerciseDirection__item {
+                        color: hotpink;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                    }
+                    .exerciseDirection__item--LT {
                         grid-area: 1 / 1 / 2 / 2;
                     }
-                    .exerciseDirection__RCItem {
+                    .exerciseDirection__item--RC {
                         grid-area: 3 / 5 / 4 / 6; 
                     }
-                    .exerciseDirection__CCItem {
+                    .exerciseDirection__item--__CC {
                         grid-area: 3 / 3 / 4 / 4; 
                     }
-                    .exerciseDirection__RTItem {
+                    .exerciseDirection__item--RT {
                         grid-area: 1 / 5 / 2 / 6; 
                     }
-                    .exerciseDirection__RBItem {
+                    .exerciseDirection__item--RB {
                         grid-area: 5 / 5 / 6 / 6; 
                     }
-                    .exerciseDirection__CBItem {
+                    .exerciseDirection__item--CB {
                         grid-area: 5 / 3 / 6 / 4; 
                     }
-                    .exerciseDirection__CTItem {
+                    .exerciseDirection__item--CT {
                         grid-area: 1 / 3 / 2 / 4; 
                     }
-                    .exerciseDirection__LCItem {
+                    .exerciseDirection__item--LC {
                         grid-area: 3 / 1 / 4 / 2; 
                     }
-                    .exerciseDirection__LBItem {
+                    .exerciseDirection__item--LB {
                         grid-area: 5 / 1 / 6 / 2; 
                     }
-                    .exerciseDirection__CCItem {
+                    .exerciseDirection__item--CC {
                         grid-area: 3 / 3 / 4 / 4; 
                     }
                     
