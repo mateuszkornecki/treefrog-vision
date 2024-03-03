b99dc4d4fd8ed70fee644dc4c87558f85ce60b9c
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2024-03-03 11:13:36 +0100

    refactor(exerciseDirection): rename radio button's class

--- src/app/exerciseConfig/components/ExerciseDirection/index.tsx
+++ src/app/exerciseConfig/components/ExerciseDirection/index.tsx
@@ -5,13 +5,13 @@ function ExerciseDirection(): ReactElement {
         <>
             <div className="exerciseTheme__container">
                 <div className="exerciseTheme__top">top</div>
-                <div className="exerciseTheme__topClick">topClick</div>
+                <div className="exerciseTheme__topRadio">topClick</div>
                 <div className="exerciseTheme__center">center</div>
-                <div className="exerciseTheme__bottomClick">bottomClick</div>
+                <div className="exerciseTheme__bottomRadio">bottomClick</div>
                 <div className="exerciseTheme__bottom">bottom</div>
                 <div className="exerciseTheme__right">right</div>
-                <div className="exerciseTheme__rightClick">rightClick</div>
-                <div className="exerciseTheme__leftClick">leftClick</div>
+                <div className="exerciseTheme__rightRadio">rightClick</div>
+                <div className="exerciseTheme__leftRadio">leftClick</div>
                 <div className="exerciseTheme__left">left</div>
             </div>
         </>
