57feffcdbcc1a74ad32152e281320b52f0aa3387
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2024-03-09 11:22:36 +0100

    refactor(PointerDirection): delete unused inport

--- src/app/exerciseConfig/components/PointerDirection/index.tsx
+++ src/app/exerciseConfig/components/PointerDirection/index.tsx
@@ -1,4 +1,4 @@
-import React, {ReactElement, useEffect, useState} from "react"
+import React, {ReactElement, useState} from "react"
 import './styles/style.css'
 
 function PointerDirection(): ReactElement {
