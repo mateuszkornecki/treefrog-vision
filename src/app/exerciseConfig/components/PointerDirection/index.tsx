a7d6384d5841a22c9ee3b24deb1facefca024dbd
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2024-03-09 11:21:10 +0100

    feat(PointerDirection): set right pointer classname onClick

--- src/app/exerciseConfig/components/PointerDirection/index.tsx
+++ src/app/exerciseConfig/components/PointerDirection/index.tsx
@@ -1,26 +1,50 @@
-import React, {ReactElement, useState} from "react"
+import React, {ReactElement, useEffect, useState} from "react"
 import './styles/style.css'
 
 function PointerDirection(): ReactElement {
 
-    type TDirection = "right" | "left" | "top" | "bottom"
+    type TDirection = "right" | "left" | "top" | "bottom" | "center"
     const [firstDirection, setFirstDirection] = useState<TDirection>()
     const [secondDirection, setSecondDirection] = useState<TDirection>()
+
+    function handlePointerClick(direction: TDirection) {
+        const hasSelectedDirections = firstDirection && secondDirection
+
+        if (hasSelectedDirections) {
+            return
+        } else {
+            if(!firstDirection) {
+                setFirstDirection(direction)
+            } else if(!secondDirection) {
+                setSecondDirection(direction)
+            }
+        }
+           }
+
+           function getPointerClassName(direction: string): string  {
+               if(direction === firstDirection || direction ===secondDirection) {
+                   return "pointerDirection__pointer pointerDirection__pointer--green"
+               } else {
+                   return "pointerDirection__pointer pointerDirection__pointer--black"
+               }
+           }
+
 
     return (
         <>
             <div className="pointerDirection__container">
                 <div className="pointerDirection__top">
                     <div
-                        className={"pointerDirection__pointer pointerDirection__pointer--black"}
-                        onClick={()=> handlePointerClick("top")}/>
+                        className={getPointerClassName("top")}
+                        onClick={()=> handlePointerClick("top")}
+                    />
                 </div>
                 <div className="pointerDirection__topTrace">
                     <div className={"pointerDirection__verticalTrace"} />
                 </div>
                 <div className="pointerDirection__center">
                     <div
-                        className={"pointerDirection__pointer pointerDirection__pointer--black"}
+                        className={getPointerClassName("center")}
                         onClick={() => handlePointerClick('center')}
                     />
                 </div>
@@ -29,13 +53,13 @@ function PointerDirection(): ReactElement {
                 </div>
                 <div className="pointerDirection__bottom">
                 <div
-                    className={"pointerDirection__pointer pointerDirection__pointer--black"}
+                    className={getPointerClassName("bottom")}
                     onClick={() => handlePointerClick("bottom")}
                 />
                 </div>
                 <div className="pointerDirection__right">
                     <div
-                        className={"pointerDirection__pointer pointerDirection__pointer--black"}
+                        className={getPointerClassName("right")}
                         onClick={() => handlePointerClick("right")}
                     />
                 </div>
@@ -43,12 +67,12 @@ function PointerDirection(): ReactElement {
                     <div className={"pointerDirection__horizontalTrace"}/>
                 </div>
                 <div className="pointerDirection__leftTrace">
-                    <div className={"pointerDirection__horizontalTrace"}/>
+                    <div className={"pointerDirection__horizontalTrace"} />
 
                 </div>
                 <div className="pointerDirection__left">
                 <div
-                    className={"pointerDirection__pointer pointerDirection__pointer--black"}
+                    className={getPointerClassName("left")}
                     onClick={() => handlePointerClick("left")}
                 />
                 </div>
